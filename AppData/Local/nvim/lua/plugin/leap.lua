60f5b4903de5e77dc1cab27a7fff2fbe5d9bedce
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-02-24 16:12:24 +0700

    add leap nvim

---
+++ AppData/Local/nvim/lua/plugin/leap.lua
@@ -0,0 +1,16 @@
+local M = {
+	"ggandor/leap.nvim",
+	commit = "ff5522a0986cca1bbbeaa4f6d86d23ebf58a025c",
+	event = "BufReadPre",
+	config = function()
+		local leap = require("leap")
+		local user = require("leap.user")
+		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward-to)")
+		vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+		leap.opts.special_keys.prev_target = "<bs>"
+		leap.opts.special_keys.prev_group = "<bs>"
+		user.set_repeat_keys("<cr>", "<bs>")
+	end,
+}
+
+return M
