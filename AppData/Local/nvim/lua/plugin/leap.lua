5e0b59bd99e66b156d9fb1ec5913300cd75fcb6c
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-03-16 17:52:28 +0700

    fix cursor bug in leap nvim

--- AppData/Local/nvim/lua/plugin/leap.lua
+++ AppData/Local/nvim/lua/plugin/leap.lua
@@ -5,11 +5,27 @@ local M = {
 	config = function()
 		local leap = require("leap")
 		local user = require("leap.user")
-		vim.keymap.set({ "n", "o" }, "s", "<Plug>(leap-forward-to)")
-		vim.keymap.set({ "n", "o" }, "S", "<Plug>(leap-backward)")
+		vim.keymap.set("n", "s", "<Plug>(leap-forward)")
+		vim.keymap.set("n", "S", "<Plug>(leap-backward)")
 		leap.opts.special_keys.prev_target = "<bs>"
 		leap.opts.special_keys.prev_group = "<bs>"
+		leap.opts.max_phase_one_targets = 0
 		user.set_repeat_keys("<cr>", "<bs>")
+		-- Hide the (real) cursor when leaping, and restore it afterwards.
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LeapEnter",
+			callback = function()
+				vim.cmd.hi("Cursor", "blend=100")
+				vim.opt.guicursor:append({ "a:Cursor/lCursor" })
+			end,
+		})
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LeapLeave",
+			callback = function()
+				vim.cmd.hi("Cursor", "blend=0")
+				vim.opt.guicursor:remove({ "a:Cursor/lCursor" })
+			end,
+		})
 	end,
 }
 
