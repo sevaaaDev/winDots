9b131472c3d07a88b3b65630ccb2ab58faf662df
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-03-16 17:52:49 +0700

    add prefix

--- AppData/Local/nvim/lua/plugin/which-key.lua
+++ AppData/Local/nvim/lua/plugin/which-key.lua
@@ -1,15 +1,17 @@
 local M = {
-  "folke/which-key.nvim",
-  commit = "5224c261825263f46f6771f1b644cae33cd06995",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
+	"folke/which-key.nvim",
+	commit = "5224c261825263f46f6771f1b644cae33cd06995",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
 }
 
 function M.config()
-  require("which-key").setup {}
+	local wk = require("which-key")
+	wk.setup({})
+	wk.register({ l = { name = "LSP" }, f = { name = "File" }, g = { name = "Git" } }, { prefix = "<leader>" })
 end
 
 return M
